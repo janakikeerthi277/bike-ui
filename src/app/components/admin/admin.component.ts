f68cd0252f6066c4e2f0ddb9c4c8f0d72370d993
Janaki Keerthi <janakikeerthi277@gmail.com>
2021-09-11 12:26:15 +0530

    angular-changes

---
+++ src/app/components/admin/admin.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { BikeService } from '../../services/bike.service'
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent implements OnInit {
+
+  public bikes : any;
+
+  constructor(private bikeService: BikeService) { }
+
+  ngOnInit(): void {
+    console.log("im here")
+    this.getBikes();
+  }
+
+  getBikes(){
+    this.bikeService.getBikes().subscribe(
+      data => { this.bikes = data },
+      err => { console.log(err) },
+      () => { console.log("Bikes Loaded!!") }
+    );
+  }
+
+}
